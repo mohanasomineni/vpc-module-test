5ec8e5f6dcbefb7c41f99f4bfb56024a1ca1c227
Mohanasomineni <mohanamanikanta7@gmail.com>
2025-02-17 10:35:18 -0500

    vpc module test

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.85.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "mohana-remote-state"
+    key    = "vpc-test" # you should have unique keys within the bucket, same key should not use in other repos or tf projects
+    region = "us-east-1"
+    dynamodb_table = "mohana-state-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
